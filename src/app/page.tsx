80bd3451e4fe22482cbc5cee4aa0a927bd064f5a
Brian Lupu <brianlupu@gmail.com>
2026-01-28 21:54:15 +0100

    tailwind and schadcn install and update deps

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 "use client";
 import { ServiceSelector } from "@/components/home/service-selector";
+import { Button } from "@/components/ui/button";
 import { useState } from "react";
 
 export default function Home() {
@@ -7,9 +8,10 @@ export default function Home() {
   return (
     <div className='flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black'>
       <main className='flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start'>
-        <h1 className='ext-red-500 text-3xl'>HOMEPAGE</h1>
+        <h1 className='text-red-500 text-3xl'>HOMEPAGE</h1>
 
         <ServiceSelector value={serviceId} onChange={setServiceId} />
+        <Button>Click me</Button>
       </main>
     </div>
   );
