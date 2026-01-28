3895b111f39ff30d96fcc74748827e9f222acbb5
Brian Lupu <brianlupu@gmail.com>
2026-01-28 20:30:14 +0100

    test

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,15 @@
-import Image from "next/image";
+"use client";
+import { ServiceSelector } from "@/components/home/service-selector";
+import { useState } from "react";
 
 export default function Home() {
+  const [serviceId, setServiceId] = useState("");
   return (
     <div className='flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black'>
       <main className='flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start'>
-        <h1>HOMEPAGE</h1>
+        <h1 className='ext-red-500 text-3xl'>HOMEPAGE</h1>
+
+        <ServiceSelector value={serviceId} onChange={setServiceId} />
       </main>
     </div>
   );
